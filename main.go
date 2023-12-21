67a93ac00fa5724454869d44fc3894f186b92c0c
Joey Chilson <joeychilson@outlook.com>
2023-12-20 23:16:28 -0600

    Added not found page.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/joeychilson/hackernews/client"
 	"github.com/joeychilson/hackernews/handlers"
+	"github.com/joeychilson/hackernews/pages"
 )
 
 //go:embed dist/*
@@ -24,8 +25,16 @@ func main() {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 
+	// Assets
 	router.Handle("/dist/*", http.FileServer(http.FS(dist)))
+
+	// Pages
 	router.HandleFunc("/", handlers.HomePage(client))
+
+	// Not Found
+	router.HandleFunc("/*", func(w http.ResponseWriter, r *http.Request) {
+		pages.NotFound().Render(r.Context(), w)
+	})
 
 	fmt.Println("Listening on http://localhost:8080")
 	http.ListenAndServe(":8080", router)
