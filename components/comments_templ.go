80574d6b9c2da45a911d491d08c00a6d53e77f7c
Joey Chilson <joeychilson@outlook.com>
2023-12-21 11:18:50 -0600

    Small fixes

--- components/comments_templ.go
+++ components/comments_templ.go
@@ -108,7 +108,7 @@ func Comments(comments []types.Item, level int) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></div><p class=\"text-sm\">")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></div><p class=\"text-sm word-wrap break-words overflow-wrap break-word\">")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
